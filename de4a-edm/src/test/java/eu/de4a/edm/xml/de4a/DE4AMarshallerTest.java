b104b9bc29873eaf5f338e6233a6735eedd695d1
Philip Helger <philip@helger.com>
2021-02-24 10:11:32 +0100

    Integrated T4.2 XSD

--- de4a-edm/src/test/java/eu/de4a/edm/xml/de4a/DE4AMarshallerTest.java
+++ de4a-edm/src/test/java/eu/de4a/edm/xml/de4a/DE4AMarshallerTest.java
@@ -19,6 +19,7 @@ import org.slf4j.LoggerFactory;
 public final class DE4AMarshallerTest
 {
   private static final Logger LOGGER = LoggerFactory.getLogger (DE4AMarshallerTest.class);
+  private static final String BASE_PATH = "src/test/resources/de4a/";
 
   private static <T> void _testReadWrite (@Nonnull final DE4AMarshaller <T> aMarshaller, @Nonnull final File aFile)
   {
@@ -40,60 +41,60 @@ public final class DE4AMarshallerTest
   @Test
   public void testDE_USI ()
   {
-    _testReadWrite (DE4AMarshaller.deUsiRequestMarshaller (), new File ("src/test/resources/de4a/DE1-USI-request.xml"));
-    _testReadWrite (DE4AMarshaller.deUsiResponseMarshaller (), new File ("src/test/resources/de4a/DE1-USI-response.xml"));
+    _testReadWrite (DE4AMarshaller.deUsiRequestMarshaller (), new File (BASE_PATH + "core/DE1-USI-request.xml"));
+    _testReadWrite (DE4AMarshaller.deUsiResponseMarshaller (), new File (BASE_PATH + "core/DE1-USI-response.xml"));
   }
 
   @Test
   public void testDO_IM ()
   {
-    _testReadWrite (DE4AMarshaller.doImRequestMarshaller (), new File ("src/test/resources/de4a/DO1-IM-request.xml"));
-    _testReadWrite (DE4AMarshaller.doImResponseMarshaller (), new File ("src/test/resources/de4a/DO1-IM-response.xml"));
+    _testReadWrite (DE4AMarshaller.doImRequestMarshaller (), new File (BASE_PATH + "core/DO1-IM-request.xml"));
+    _testReadWrite (DE4AMarshaller.doImResponseMarshaller (), new File (BASE_PATH + "core/DO1-IM-response.xml"));
   }
 
   @Test
   public void testDO_USI ()
   {
-    _testReadWrite (DE4AMarshaller.doUsiRequestMarshaller (), new File ("src/test/resources/de4a/DO1-USI-request.xml"));
-    _testReadWrite (DE4AMarshaller.doUsiResponseMarshaller (), new File ("src/test/resources/de4a/DO1-USI-response.xml"));
+    _testReadWrite (DE4AMarshaller.doUsiRequestMarshaller (), new File (BASE_PATH + "core/DO1-USI-request.xml"));
+    _testReadWrite (DE4AMarshaller.doUsiResponseMarshaller (), new File (BASE_PATH + "core/DO1-USI-response.xml"));
   }
 
   @Test
   public void testDR_IM ()
   {
-    _testReadWrite (DE4AMarshaller.drImRequestMarshaller (), new File ("src/test/resources/de4a/DR1-IM-request.xml"));
-    _testReadWrite (DE4AMarshaller.drImResponseMarshaller (), new File ("src/test/resources/de4a/DR1-IM-response.xml"));
+    _testReadWrite (DE4AMarshaller.drImRequestMarshaller (), new File (BASE_PATH + "core/DR1-IM-request.xml"));
+    _testReadWrite (DE4AMarshaller.drImResponseMarshaller (), new File (BASE_PATH + "core/DR1-IM-response.xml"));
   }
 
   @Test
   public void testDR_USI ()
   {
-    _testReadWrite (DE4AMarshaller.drUsiRequestMarshaller (), new File ("src/test/resources/de4a/DR1-USI-request.xml"));
-    _testReadWrite (DE4AMarshaller.drUsiResponseMarshaller (), new File ("src/test/resources/de4a/DR1-USI-response.xml"));
+    _testReadWrite (DE4AMarshaller.drUsiRequestMarshaller (), new File (BASE_PATH + "core/DR1-USI-request.xml"));
+    _testReadWrite (DE4AMarshaller.drUsiResponseMarshaller (), new File (BASE_PATH + "core/DR1-USI-response.xml"));
   }
 
   @Test
   public void testDT_USI ()
   {
-    _testReadWrite (DE4AMarshaller.dtUsiRequestMarshaller (), new File ("src/test/resources/de4a/DT1-USI-request.xml"));
-    _testReadWrite (DE4AMarshaller.dtUsiResponseMarshaller (), new File ("src/test/resources/de4a/DT1-USI-response.xml"));
+    _testReadWrite (DE4AMarshaller.dtUsiRequestMarshaller (), new File (BASE_PATH + "core/DT1-USI-request.xml"));
+    _testReadWrite (DE4AMarshaller.dtUsiResponseMarshaller (), new File (BASE_PATH + "core/DT1-USI-response.xml"));
   }
 
   @Test
   public void testIDK_LookupEvidenceServiceData ()
   {
     _testReadWrite (DE4AMarshaller.idkRequestLookupEvidenceServiceDataMarshaller (),
-                    new File ("src/test/resources/de4a/DR-DT1-IDK-request-evidence.xml"));
+                    new File (BASE_PATH + "core/DR-DT1-IDK-request-evidence.xml"));
     _testReadWrite (DE4AMarshaller.idkResponseLookupEvidenceServiceDataMarshaller (),
-                    new File ("src/test/resources/de4a/DR-DT1-IDK-response-evidence.xml"));
+                    new File (BASE_PATH + "core/DR-DT1-IDK-response-evidence.xml"));
   }
 
   @Test
   public void testIDK_LookupRoutingInformation ()
   {
     _testReadWrite (DE4AMarshaller.idkRequestLookupRoutingInformationMarshaller (),
-                    new File ("src/test/resources/de4a/DR-DT1-IDK-request-routing.xml"));
+                    new File (BASE_PATH + "core/DR-DT1-IDK-request-routing.xml"));
     _testReadWrite (DE4AMarshaller.idkResponseLookupRoutingInformationMarshaller (),
-                    new File ("src/test/resources/de4a/DR-DT1-IDK-response-routing.xml"));
+                    new File (BASE_PATH + "core/DR-DT1-IDK-response-routing.xml"));
   }
 }
