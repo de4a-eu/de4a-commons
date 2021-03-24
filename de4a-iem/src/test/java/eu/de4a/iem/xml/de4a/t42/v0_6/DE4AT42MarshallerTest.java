5dbf59a74d6c90ba54904d1c05e756b20d22abef
Philip Helger <philip@helger.com>
2021-03-24 15:56:16 +0100

    Adopted according to WP3

--- de4a-iem/src/test/java/eu/de4a/iem/xml/de4a/t42/v0_6/DE4AT42MarshallerTest.java
+++ de4a-iem/src/test/java/eu/de4a/iem/xml/de4a/t42/v0_6/DE4AT42MarshallerTest.java
@@ -56,8 +56,8 @@ public final class DE4AT42MarshallerTest
   @Test
   public void testLegalEntity ()
   {
-    _testReadWrite (DE4AT42Marshaller.legalEntity (), new File (BASE_PATH + "sample CompanyInfo NL KVK.xml"));
-    _testReadWrite (DE4AT42Marshaller.legalEntity (), new File (BASE_PATH + "sample CompanyInfo RO ONRC-3.xml"));
-    _testReadWrite (DE4AT42Marshaller.legalEntity (), new File (BASE_PATH + "sample company info SE -2.xml"));
+    _testReadWrite (DE4AT42Marshaller.legalEntity (), new File (BASE_PATH + "Sample Company Registration NL KVK.xml"));
+    _testReadWrite (DE4AT42Marshaller.legalEntity (), new File (BASE_PATH + "Sample Company Registration RO ONRC.xml"));
+    _testReadWrite (DE4AT42Marshaller.legalEntity (), new File (BASE_PATH + "Sample Company Registration SE.xml"));
   }
 }
