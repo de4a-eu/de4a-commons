beec663afece2449cd8397d1ef22bd87704fe297
Philip Helger <philip@helger.com>
2021-05-16 22:35:12 +0200

    Started Moving Abroad XSDs

--- de4a-iem/src/main/java/eu/de4a/iem/xml/de4a/CDE4AJAXB.java
+++ de4a-iem/src/main/java/eu/de4a/iem/xml/de4a/CDE4AJAXB.java
@@ -22,6 +22,7 @@ import com.helger.commons.collection.impl.ICommonsList;
 import com.helger.commons.io.resource.ClassPathResource;
 import com.helger.ubl20.CUBL20;
 import com.helger.ubl23.CUBL23;
+import com.helger.xsds.ccts.cct.schemamodule.CCCTS;
 import com.helger.xsds.xml.CXML_XSD;
 
 /**
@@ -46,6 +47,7 @@ public final class CDE4AJAXB
     a.add (CXML_XSD.getXSDResource ());
     a.add (new ClassPathResource ("schemas/core/de4a/common-identity-types.xsd", _getCL ()));
     a.add (CUBL20.XSD_CODELIST_MIME_MEDIA_TYPE_CODE);
+    a.add (CCCTS.getXSDResource ());
     a.add (CUBL23.XSD_UNQUALIFIED_DATA_TYPES);
     a.add (new ClassPathResource ("schemas/core/de4a/common-types.xsd", _getCL ()));
     XSDS = a.getAsUnmodifiable ();
@@ -59,7 +61,8 @@ public final class CDE4AJAXB
   public static final ClassPathResource XSD_DR_DE_IM = new ClassPathResource ("schemas/core/DR-DE1-IM.xsd", _getCL ());
   public static final ClassPathResource XSD_DT_DO_IM = new ClassPathResource ("schemas/core/DT-DO1-IM.xsd", _getCL ());
 
-  public static final ClassPathResource XSD_DR_DT_IDK = new ClassPathResource ("schemas/core/DR-DT1-IDK.xsd", _getCL ());
+  public static final ClassPathResource XSD_DR_DT_IDK = new ClassPathResource ("schemas/core/DR-DT1-IDK.xsd",
+                                                                               _getCL ());
 
   private CDE4AJAXB ()
   {}
--- de4a-iem/src/main/java/eu/de4a/iem/xml/de4a/t42/v0_6/CT42.java
+++ de4a-iem/src/main/java/eu/de4a/iem/xml/de4a/t42/v0_6/CT42.java
@@ -20,6 +20,7 @@ import com.helger.commons.collection.impl.CommonsArrayList;
 import com.helger.commons.collection.impl.ICommonsList;
 import com.helger.commons.io.resource.ClassPathResource;
 import com.helger.ubl23.CUBL23;
+import com.helger.xsds.ccts.cct.schemamodule.CCCTS;
 import com.helger.xsds.xml.CXML_XSD;
 
 /**
@@ -46,6 +47,7 @@ public final class CT42
   {
     final ICommonsList <ClassPathResource> a = new CommonsArrayList <> ();
     a.add (CXML_XSD.getXSDResource ());
+    a.add (CCCTS.getXSDResource ());
     a.add (CUBL23.XSD_UNQUALIFIED_DATA_TYPES);
     a.add (new ClassPathResource ("schemas/pilot-shared/CoreVocabularies-BasicComponents-1.1.xsd", _getCL ()));
     a.add (new ClassPathResource ("schemas/t4.2/doing_Business_abroad_XSD_v0.6 draft.xsd", _getCL ()));
