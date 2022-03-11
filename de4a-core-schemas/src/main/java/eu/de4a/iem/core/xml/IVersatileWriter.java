f857bc8704ab0768674a1a5f545660de2ac47dba
Philip Helger <philip@helger.com>
2022-03-11 10:03:40 +0100

    Changed package name

--- de4a-core-schemas/src/main/java/eu/de4a/iem/core/xml/IVersatileWriter.java
+++ de4a-core-schemas/src/main/java/eu/de4a/iem/core/xml/IVersatileWriter.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package eu.de4a.iem.xml;
+package eu.de4a.iem.core.xml;
 
 import java.io.File;
 import java.io.OutputStream;
