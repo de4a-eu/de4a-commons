e642f60dc870d9afde8a8378c00f4b56fd1bd059
Philip Helger <philip@helger.com>
2022-05-29 18:19:41 +0200

    Changed `eu.de4a.iem.cev.de4a.t43.v1_6b` to `eu.de4a.iem.cev.de4a.t43`

--- de4a-canonical-evidences/src/test/java/eu/de4a/iem/cev/de4a/t43/CT43Test.java
+++ de4a-canonical-evidences/src/test/java/eu/de4a/iem/cev/de4a/t43/CT43Test.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package eu.de4a.iem.cev.de4a.t43.v1_6b;
+package eu.de4a.iem.cev.de4a.t43;
 
 import static org.junit.Assert.assertTrue;
 
