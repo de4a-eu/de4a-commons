749c36f35e85c67c5a6939bcd85529f2f1281e6a
Philip Helger <philip@helger.com>
2022-05-30 20:49:16 +0200

    Working on license headers

--- de4a-core-schemas/src/test/java/eu/de4a/iem/core/DE4ACoreMarshallerTest.java
+++ de4a-core-schemas/src/test/java/eu/de4a/iem/core/DE4ACoreMarshallerTest.java
@@ -1,29 +1,3 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/**
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package eu.de4a.iem.core;
 
 import static org.junit.Assert.assertNotNull;
@@ -51,7 +25,8 @@ public final class DE4ACoreMarshallerTest
   private static final String BASE_PATH = "src/test/resources/de4a/";
 
   @SuppressWarnings ("unused")
-  private static <T> void _receiveViaHttp (@Nonnull final GenericJAXBMarshaller <T> aMarshaller, @Nonnull final File aFile) throws Exception
+  private static <T> void _receiveViaHttp (@Nonnull final GenericJAXBMarshaller <T> aMarshaller,
+                                           @Nonnull final File aFile) throws Exception
   {
     final Wrapper <Exception> aExWrapper = new Wrapper <> ();
     aMarshaller.readExceptionCallbacks ().removeAll ();
@@ -67,7 +42,8 @@ public final class DE4ACoreMarshallerTest
     }
   }
 
-  private static <T> void _testReadWrite (@Nonnull final GenericJAXBMarshaller <T> aMarshaller, @Nonnull final File aFile)
+  private static <T> void _testReadWrite (@Nonnull final GenericJAXBMarshaller <T> aMarshaller,
+                                          @Nonnull final File aFile)
   {
     assertTrue ("Test file does not exists " + aFile.getAbsolutePath (), aFile.exists ());
 
